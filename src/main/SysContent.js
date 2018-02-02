877670a5c6ea5efc2d194a0d575a8bccfc39e23d
大無 <aizgw2012@163.com>
2018-02-02 09:18:23 +0800

    报障项目初始化

--- src/main/SysContent.js
+++ src/main/SysContent.js
@@ -1,32 +1,20 @@
 import React, {Component } from 'react';
 import { Layout, Breadcrumb } from 'antd';
 import 'antd/dist/antd.css';
-import Records from '../components/records/Records';
+import Maintain from '../components/maintain/Maintain';
 
 const { Content } = Layout;
-const { BreadcrumbItem } = Breadcrumb.Item;
 
 class SysContent extends Component{
     render(){
         console.log(this.props.meunType);
         const keyPaths = this.props.meunType.keyPath;
-        const routes = [{
-            path: 'index',
-            breadcrumbName: '首页'
-        }, {
-            path: 'first',
-            breadcrumbName: '一级面包屑'
-        }, {
-            path: 'second',
-            breadcrumbName: '当前页面'
-        }];
 
         return(
             <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '12px 0' }} routes={routes}>
-                </Breadcrumb>
+                <Breadcrumb style={{ margin: '12px 0' }} routes={keyPaths}></Breadcrumb>
                 <div style={{ padding: 24, background: '#fff', minHeight: 780 }}>
-                    <Records />
+                    <Maintain />
                 </div>
             </Content>
         );
